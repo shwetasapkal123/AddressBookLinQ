073957211b9644d42cec3fc73fd74cac595b75a6
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-03-05 13:27:41 +0530

    [shweta] Wel-Come Message

---
+++ AddressBookLinQ/AddressBookLinQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLinQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WEL-COME to LinQ of AddressBook");
+            Console.ReadLine();
+        }
+    }
+}
